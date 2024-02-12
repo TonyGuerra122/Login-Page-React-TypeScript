f1599cf9e317d38aff95be1734b1cfaa7cdd93a5
TonyGuerra122 <guerra.anthony122@gmail.com>
2024-02-12 15:06:25 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "./components/LoginForm/LoginForm";
+
+const App:React.FC = () => {
+  return(
+    <div className="App">
+      <LoginForm/>
+    </div>
+  )
+}
+export default App;
